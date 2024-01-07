c6fb9db01c8317fe3d73ab52a27ad84700729edc
Artem <mstrjesm@gmail.com>
2024-01-07 22:47:26 +0200

    feat(container-base): add component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <button-base
-      @click="visible = true"
-    >
-      Open {{ visible }}
-    </button-base>
+    <container-base>
+      <button-base
+        @click="visible = true"
+      >
+        Open {{ visible }}
+      </button-base>
 
-    <button-base
-      @click="visible = true"
-      variant="secondary"
-    >
-      Open {{ visible }}
-    </button-base>
-    <modal-window
-      v-model="visible"
-      title="Some title"
-    >
-      Some content
-    </modal-window>
-    <timeline-calendar />
+      <button-base
+        @click="visible = true"
+        variant="secondary"
+      >
+        Open {{ visible }}
+      </button-base>
+      <modal-window
+        v-model="visible"
+        title="Some title"
+      >
+        Some content
+      </modal-window>
+      <timeline-calendar />
+    </container-base>
   </div>
 </template>
 
@@ -26,6 +28,7 @@
 import ButtonBase from '@/components/buttons/ButtonBase.vue';
 import ModalWindow from '@/components/modals/ModalWindow.vue';
 import TimelineCalendar from '@/entities/TimelineCalendar/TimelineCalendar.vue';
+import ContainerBase from './components/container/ContainerBase.vue';
 
 export default {
   name: 'App',
@@ -33,6 +36,7 @@ export default {
     ModalWindow,
     ButtonBase,
     TimelineCalendar,
+    ContainerBase,
   },
 
   data() {
---
+++ src/components/container/ContainerBase.vue
@@ -0,0 +1,26 @@
+<template
+  functional
+>
+  <div
+    class="container"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContainerBase',
+};
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 var(--space-medium);
+}
+</style>
